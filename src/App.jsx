46181cce69252b408314421a12e8f03fad0d5016
Salman7D <salmanrayhan753@gmail.com>
2024-03-16 00:06:09 +0600

    foodbar added

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,185 @@ function App() {
       <Header></Header>
       <Banner></Banner>
       <Our></Our>
+
+      <div className='grid grid-cols-2 gap-0'>
+      <div className='rounded-2xl border border-solid border-[#28282833] w-[380px] p-6'>
+        <img className='mb-6' src="/public/Images/Rectangle 20170.png" alt="" />
+        <p className='mb-4'>Spaghetti Bolognese</p>
+        <p className='mb-10'>Classic Italian pasta dish with savory meat sauce.</p>
+        <div className='bg-[#2828281A] h-[1px] w-[330px]'>
+
+        </div>
+        <p className='mb-1'>Ingredients: 1</p>
+        <p className='mb-4'>500g</p>
+        <div className='bg-[#2828281A] h-[1px] w-[330px] mb-6'>
+
+        </div>
+        <div className='flex gap-3 mb-6'>
+          <div className='flex'>
+            <img src="/public/Images/Clock.png" alt="" />
+            <p>time</p>
+          </div>
+          <div className='flex'>
+            <img src="/public/Images/Fire.png" alt="" />
+            <p>calories</p>
+          </div>
+        </div>
+
+        <button className="btn btn-success">Success</button>
+
+
+      </div>
+
+      <div className='rounded-2xl border border-solid border-[#28282833] w-[380px] p-6'>
+        <img className='mb-6' src="/public/Images/Rectangle 20170.png" alt="" />
+        <p className='mb-4'>Spaghetti Bolognese</p>
+        <p className='mb-10'>Classic Italian pasta dish with savory meat sauce.</p>
+        <div className='bg-[#2828281A] h-[1px] w-[330px]'>
+
+        </div>
+        <p className='mb-1'>Ingredients: 1</p>
+        <p className='mb-4'>500g</p>
+        <div className='bg-[#2828281A] h-[1px] w-[330px] mb-6'>
+
+        </div>
+        <div className='flex gap-3 mb-6'>
+          <div className='flex'>
+            <img src="/public/Images/Clock.png" alt="" />
+            <p>time</p>
+          </div>
+          <div className='flex'>
+            <img src="/public/Images/Fire.png" alt="" />
+            <p>calories</p>
+          </div>
+
+          
+        </div>
+
+        <button className="btn btn-success">Success</button>
+
+
+      </div>
+      <div className='rounded-2xl border border-solid border-[#28282833] w-[380px] p-6'>
+        <img className='mb-6' src="/public/Images/Rectangle 20170.png" alt="" />
+        <p className='mb-4'>Spaghetti Bolognese</p>
+        <p className='mb-10'>Classic Italian pasta dish with savory meat sauce.</p>
+        <div className='bg-[#2828281A] h-[1px] w-[330px]'>
+
+        </div>
+        <p className='mb-1'>Ingredients: 1</p>
+        <p className='mb-4'>500g</p>
+        <div className='bg-[#2828281A] h-[1px] w-[330px] mb-6'>
+
+        </div>
+        <div className='flex gap-3 mb-6'>
+          <div className='flex'>
+            <img src="/public/Images/Clock.png" alt="" />
+            <p>time</p>
+          </div>
+          <div className='flex'>
+            <img src="/public/Images/Fire.png" alt="" />
+            <p>calories</p>
+          </div>
+
+          
+        </div>
+
+        <button className="btn btn-success">Success</button>
+
+
+      </div>
+      <div className='rounded-2xl border border-solid border-[#28282833] w-[380px] p-6'>
+        <img className='mb-6' src="/public/Images/Rectangle 20170.png" alt="" />
+        <p className='mb-4'>Spaghetti Bolognese</p>
+        <p className='mb-10'>Classic Italian pasta dish with savory meat sauce.</p>
+        <div className='bg-[#2828281A] h-[1px] w-[330px]'>
+
+        </div>
+        <p className='mb-1'>Ingredients: 1</p>
+        <p className='mb-4'>500g</p>
+        <div className='bg-[#2828281A] h-[1px] w-[330px] mb-6'>
+
+        </div>
+        <div className='flex gap-3 mb-6'>
+          <div className='flex'>
+            <img src="/public/Images/Clock.png" alt="" />
+            <p>time</p>
+          </div>
+          <div className='flex'>
+            <img src="/public/Images/Fire.png" alt="" />
+            <p>calories</p>
+          </div>
+
+          
+        </div>
+
+        <button className="btn btn-success">Success</button>
+
+
+      </div>
+      <div className='rounded-2xl border border-solid border-[#28282833] w-[380px] p-6'>
+        <img className='mb-6' src="/public/Images/Rectangle 20170.png" alt="" />
+        <p className='mb-4'>Spaghetti Bolognese</p>
+        <p className='mb-10'>Classic Italian pasta dish with savory meat sauce.</p>
+        <div className='bg-[#2828281A] h-[1px] w-[330px]'>
+
+        </div>
+        <p className='mb-1'>Ingredients: 1</p>
+        <p className='mb-4'>500g</p>
+        <div className='bg-[#2828281A] h-[1px] w-[330px] mb-6'>
+
+        </div>
+        <div className='flex gap-3 mb-6'>
+          <div className='flex'>
+            <img src="/public/Images/Clock.png" alt="" />
+            <p>time</p>
+          </div>
+          <div className='flex'>
+            <img src="/public/Images/Fire.png" alt="" />
+            <p>calories</p>
+          </div>
+
+          
+        </div>
+
+        <button className="btn btn-success">Success</button>
+
+
+      </div>
+      <div className='rounded-2xl border border-solid border-[#28282833] w-[380px] p-6'>
+        <img className='mb-6' src="/public/Images/Rectangle 20170.png" alt="" />
+        <p className='mb-4'>Spaghetti Bolognese</p>
+        <p className='mb-10'>Classic Italian pasta dish with savory meat sauce.</p>
+        <div className='bg-[#2828281A] h-[1px] w-[330px]'>
+
+        </div>
+        <p className='mb-1'>Ingredients: 1</p>
+        <p className='mb-4'>500g</p>
+        <div className='bg-[#2828281A] h-[1px] w-[330px] mb-6'>
+
+        </div>
+        <div className='flex gap-3 mb-6'>
+          <div className='flex'>
+            <img src="/public/Images/Clock.png" alt="" />
+            <p>time</p>
+          </div>
+          <div className='flex'>
+            <img src="/public/Images/Fire.png" alt="" />
+            <p>calories</p>
+          </div>
+
+          
+        </div>
+
+        <button className="btn btn-success">Success</button>
+
+
+      </div>
+      </div>
+      
+
+
       
     </>
   )
--- src/assets/Components/Our/Our.jsx
+++ src/assets/Components/Our/Our.jsx
@@ -3,7 +3,6 @@ const Our = () => {
         <div className="text-center">
             <p className="mb-6">Our Recipes</p>
             <p className="mb-12">Welcome to our recipes store. Here you are find a lot of different types of food you want. <br /> Just search your favorite food name and enjoy it.</p>
-
         </div>
     );
 };
